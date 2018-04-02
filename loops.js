fb0b58444435d858fba7fffea17833d1d311ad4b
Dennis Lord <lorddennis1006@gmail.com>
2018-04-02 12:48:16 +0000

    automatically backed up by learn

--- loops.js
+++
@@ -1,9 +0,0 @@
-console.log("1");
-console.log("2");
-console.log("3");
-console.log("4");
-console.log("5");
-
-for (var counter = 1; counter < 11; counter++) {
-	console.log(counter);}
-for (var i = 5; i < 11; i = i + 1){console.log(i);}
